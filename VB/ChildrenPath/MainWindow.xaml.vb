fdbf947e746da21960ecd8bfb34c5e8f70be6511
DevExpressExampleBot <fakeEmail>
2022-12-16 19:17:41 +0400

    Source auto update [skip ci]

--- VB/ChildrenPath/MainWindow.xaml.vb
+++ VB/ChildrenPath/MainWindow.xaml.vb
@@ -1,15 +1,15 @@
-﻿Imports DevExpress.Xpf.Core
-
+Imports DevExpress.Xpf.Core
 
 Namespace ChildrenPath
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits DXWindow
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
--- VB/ChildrenPath/ViewModel.vb
+++ VB/ChildrenPath/ViewModel.vb
@@ -1,48 +1,41 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 
 Namespace ChildrenPath
 
     Friend Class ViewModel
-        Public Property AppMenu() As Menu
-        Public Property SelectedItem() As MenuItem
+
+        Public Property AppMenu As Menu
+
+        Public Property SelectedItem As MenuItem
+
         Public Sub New()
-            AppMenu = New Menu()
-            SelectedItem = AppMenu.MenuItems(0)
+            Me.AppMenu = New ChildrenPath.Menu()
+            Me.SelectedItem = Me.AppMenu.MenuItems(0)
         End Sub
     End Class
 
     Public Class Menu
-        Public Property MenuItems() As List(Of MenuItem)
+
+        Public Property MenuItems As List(Of ChildrenPath.MenuItem)
+
         Public Sub New()
-            MenuItems = GetMenuItems()
+            Me.MenuItems = ChildrenPath.Menu.GetMenuItems()
         End Sub
-        Private Shared Function GetMenuItems() As List(Of MenuItem)
-            Dim items As New List(Of MenuItem)()
-            Dim subitems As New List(Of MenuItem)()
-            subitems.Add(New MenuItem() With {.Caption = "SubItem3"})
-            items.Add(New MenuItem() With { _
-                .Caption = "Item1", _
-                .SubItems = New List(Of MenuItem)() From { _
-                    New MenuItem() With {.Caption = "SubItem1"}, _
-                    New MenuItem() With { _
-                        .Caption = "SubItem2", _
-                        .SubItems=subitems _
-                    } _
-                } _
-            })
-            items.Add(New MenuItem() With { _
-                .Caption = "Item2", _
-                .SubItems = New List(Of MenuItem)() From { _
-                    New MenuItem() With {.Caption = "SubItem1"}, _
-                    New MenuItem() With {.Caption = "SubItem2"} _
-                } _
-            })
+
+        Private Shared Function GetMenuItems() As List(Of ChildrenPath.MenuItem)
+            Dim items As System.Collections.Generic.List(Of ChildrenPath.MenuItem) = New System.Collections.Generic.List(Of ChildrenPath.MenuItem)()
+            Dim subitems As System.Collections.Generic.List(Of ChildrenPath.MenuItem) = New System.Collections.Generic.List(Of ChildrenPath.MenuItem)()
+            subitems.Add(New ChildrenPath.MenuItem() With {.Caption = "SubItem3"})
+            items.Add(New ChildrenPath.MenuItem() With {.Caption = "Item1", .SubItems = New System.Collections.Generic.List(Of ChildrenPath.MenuItem)() From {New ChildrenPath.MenuItem() With {.Caption = "SubItem1"}, New ChildrenPath.MenuItem() With {.Caption = "SubItem2", .SubItems = subitems}}})
+            items.Add(New ChildrenPath.MenuItem() With {.Caption = "Item2", .SubItems = New System.Collections.Generic.List(Of ChildrenPath.MenuItem)() From {New ChildrenPath.MenuItem() With {.Caption = "SubItem1"}, New ChildrenPath.MenuItem() With {.Caption = "SubItem2"}}})
             Return items
         End Function
     End Class
 
     Public Class MenuItem
-        Public Property Caption() As String
-        Public Property SubItems() As List(Of MenuItem)
+
+        Public Property Caption As String
+
+        Public Property SubItems As List(Of ChildrenPath.MenuItem)
     End Class
 End Namespace
